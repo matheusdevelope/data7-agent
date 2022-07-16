529a1ce331cf7538eaddac6c5670203995ae5ee8
Matheus Teixeira <emersonmatheustu@gmail.com>
2022-07-16 10:27:14 -0400

    add verification on POST request

--- src/server/controllers/QrCode.ts
+++ src/server/controllers/QrCode.ts
@@ -7,6 +7,22 @@ async function get(req: Request, res: Response) {
 
 async function post(req: Request, res: Response) {
   const qrcode = req.body;
+  function BadRequest(message: string) {
+    return res.status(400).send(message);
+  }
+
+  const { action, id, awaiting_payment, confirmed_payment, canceled, img } = qrcode;
+  if (!action) return BadRequest('action not provided');
+  if (action !== 'open' && action !== 'update' && action !== 'close') return BadRequest('invalid action');
+  if (!id) return BadRequest('id not provided');
+  if (awaiting_payment == undefined) return BadRequest('awaiting_payment not provided');
+  if (awaiting_payment !== true && awaiting_payment !== false) return BadRequest('awaiting_payment must be a boolean');
+  if (confirmed_payment == undefined) return BadRequest('confirmed_payment not provided');
+  if (confirmed_payment !== true && confirmed_payment !== false)
+    return BadRequest('awaiting_payment must be a boolean');
+  if (canceled == undefined) return BadRequest('canceled not provided');
+  if (canceled !== true && canceled !== false) return BadRequest('awaiting_payment must be a boolean');
+  if (!img && action !== 'close') return BadRequest('img not provided');
 
   CallQrCode({
     qrcode: qrcode,
